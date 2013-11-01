d6119e290ff2d78d37ec63ef208c326283e44a24
Hubert Wong <hubertwwong@gmail.com>
2013-11-01 16:09:01 -0700

    added the necessary fb html for buttons to get things to work.

--- public/js/fb.js
+++ public/js/fb.js
@@ -12,11 +12,13 @@ window.fbAsyncInit = function() {
 	// whenever someone who was previously logged out tries to log in again, the correct case below
 	// will be handled.
 	FB.Event.subscribe('auth.authResponseChange', function(response) {
+		console.log("start of auth response");
 		// Here we specify what we do with the response anytime this event occurs.
 		if (response.status === 'connected') {
 			// The response object is returned with a status field that lets the app know the current
 			// login status of the person. In this case, we're handling the situation where they
 			// have logged in to the app.
+			console.log("connected");
 			testAPI();
 		} else if (response.status === 'not_authorized') {
 			// In this case, the person is logged into Facebook, but not into the app, so we call
@@ -26,6 +28,7 @@ window.fbAsyncInit = function() {
 			// (1) JavaScript created popup windows are blocked by most browsers unless they
 			// result from direct interaction from people using the app (such as a mouse click)
 			// (2) it is a bad experience to be continually prompted to login upon page load.
+			console.log("not_authorized");
 			FB.login();
 		} else {
 			// In this case, the person is not logged into Facebook, so we call the login()
@@ -33,6 +36,7 @@ window.fbAsyncInit = function() {
 			// of whether they are logged into the app. If they aren't then they'll see the Login
 			// dialog right after they log in to Facebook.
 			// The same caveats as above apply to the FB.login() call here.
+			console.log("else");
 			FB.login();
 		}
 	});
@@ -40,16 +44,17 @@ window.fbAsyncInit = function() {
 
 // Load the SDK asynchronously
 ( function(d) {
-		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-		if (d.getElementById(id)) {
-			return;
-		}
-		js = d.createElement('script');
-		js.id = id;
-		js.async = true;
-		js.src = "http://connect.facebook.net/en_US/all.js";
-		ref.parentNode.insertBefore(js, ref);
-	}(document));
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement('script');
+	js.id = id;
+	js.async = true;
+	js.src = "http://connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+	console.log("sdk loaded");
+}(document));
 
 // Here we run a very simple test of the Graph API after login is successful.
 // This testAPI() function is only called in those cases.
